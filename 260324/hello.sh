e6b06e79bc033ca76ff3bd4cf3045c1cdef3aa3c
Enrico Weiser <enrico.weiser@htl-braunau.at>
2026-03-24 08:44:26 +0100

    24.03.2026.1

--- 260324/hello.sh
+++ 260324/hello.sh
@@ -4,4 +4,31 @@
 #Shell Script = Textdatei mit shell Befehlen
 #Befehle ergeben das gleiche Ergebnis wie in der Shell ausgefuehrt
 
-echo "Hallo Welt"
+echo $MYVAR WELT
+echo Guten Morgen
+
+
+FILE_NAME=TEST.txt
+DIR_NAME=mydir
+#mkdir "$DIR_NAME"
+
+echo "Irgendein Text" > "$DIR_NAME/$FILE_NAME"
+
+
+#double quotes ""
+NAME="Enrico"
+TEXT="HALLO $NAME"
+
+echo $TEXT
+
+#single quotes ''
+TEXT2='HALLO $NAME'
+echo $TEXT2
+
+#curly braces {}
+FILE=myname
+#echo "$FILE_001.txt"
+#echo "$FILE_002.txt"
+
+echo "${FILE}_001.txt"
+echo "${FILE}_002.txt"
